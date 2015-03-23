8b9a5fef70902eef836a7706011255520b943475
Lyosha Kuleshov <aleksey.kuleshov@flocktory.com>
2015-03-23 11:01:42 +0300

    refactoring

--- src/skovajsa/launchpad.clj
+++ src/skovajsa/launchpad.clj
@@ -4,24 +4,48 @@
             [com.stuartsierra.component :as component]
             [skovajsa.launchpad.events :as events]
             [skovajsa.launchpad.led :as led]
-            [skovajsa.launchpad.grid :as grid]))
+            [skovajsa.launchpad.grid :as grid]
+            [clojure.tools.logging :as log]))
+
+(defn get-btn
+  [lp x y]
+  (get @(:grid lp) [x y]))
+
+(defn upd-btn!
+  [lp x y val vel]
+  (swap! (:grid lp) assoc [x y] {:val val :vel vel}))
+
+(defn connect-launchpad!
+  []
+  (let [d (midi/midi-find-connected-device "Launchpad")
+        r (midi/midi-find-connected-receiver "Launchpad")]
+    (do
+      (if d
+        (log/info "Launchpad device connected.")
+        (log/warn "Failed to connect Launchpad device."))
+      (if r
+        (log/info "Launchpad receiver connected.")
+        (log/warn "Failed to connect Launchpad receiver."))
+      {:dvc d :rcv r})))
 
 (defrecord Launchpad [dvc rcv grid handlers mode]
   component/Lifecycle
   (start [this]
-    (let [d (midi/midi-find-connected-device "Launchpad")
-          r (midi/midi-find-connected-receiver "Launchpad")]
-      (events/bind-all! {:dvc d :rcv r})
-      (assoc this :dvc d :rcv r :grid (atom grid/init-grid))))
+    (let [lp (connect-launchpad!)]
+      (events/bind-all! lp)
+      (assoc this :dvc (:dvc lp)
+                  :rcv (:rcv lp)
+                  :grid (atom grid/init-grid)
+                  :handlers nil
+                  :mode nil)))
   (stop [this]
     (events/unbind-all! this)
-    (led/all-led-off (:rcv this))
-    this)
+    (led/all-led-off (:rcv this)))
   grid/Grid
   (get-btn [_ x y]
-    (grid/get-btn grid x y))
+    (get-btn grid x y))
   (upd-btn! [_ x y val vel]
-    (grid/upd-btn! grid x y val vel)))
+    (upd-btn! grid x y val vel)))
 
 (defn new-launchpad []
   (map->Launchpad {}))
--- src/skovajsa/launchpad/grid.clj
+++ src/skovajsa/launchpad/grid.clj
@@ -9,14 +9,6 @@
                     (for [i (range 1 9) j (range 1 9)] [i j])
                     (repeat nil))))
 
-(defn get-btn
-  [grid x y]
-  (get @grid [x y]))
-
-(defn upd-btn!
-  [grid x y val vel]
-  (swap! grid assoc [x y] {:val val :vel vel}))
-
 (defn note-num
   "Takes x and y coordinates of a button, 1 through 8,
   returns the note number from 0 to 127."
