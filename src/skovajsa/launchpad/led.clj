8152cea246a4e07d8975ab3eab8d1628d21d34ca
Lyosha Kuleshov <aleksey.kuleshov@flocktory.com>
2015-05-14 15:19:58 +0300

    separate lp-print

--- src/skovajsa/launchpad/led.clj
+++ src/skovajsa/launchpad/led.clj
@@ -48,23 +48,6 @@
         (let [c (get new-grid xy)]
           (if (nil? c) (btn-off lp xy) (btn-on lp xy c)))))))
 
-(defn lp-print
-  [lp notes vel dur]
-  (doseq [n notes]
-    (midi/midi-note (:rcv lp) n vel dur)))
-
-(def char-map
-  {\e [1 2 2 3 4 5 6 17 18 33 34 49 50 50 51 52 53 65 66 81 82 97 98 113 114 114 115 116 117 118]
-   \k [1 2 6 17 18 21 33 34 36 49 50 51 65 66 67 81 82 84 97 98 101 113 114 118]
-   \t [1 2 3 4 5 6 17 18 19 20 21 22 35 36 51 52 67 68 83 84 99 100 115 116]
-   \o [3 4 18 21 33 33 38 38 49 54 65 70 81 86 98 101 115 116]})
-
-(defn lp-print-str
-  [lp str color dur]
-  (doseq [c str]
-    (lp-print (:rcv lp) (get char-map c) (utils/color->vel color) dur)
-    (Thread/sleep dur)))
-
 (defn control-led-on
   "Turns on a round control button in the top row.
   Accepts a Launchpad component, a mode keyword, and an optional
@@ -76,6 +59,3 @@
       (midi/midi-control (:rcv lp)
                          (utils/control->note control)
                          (utils/color->vel color)))))
-
-(comment
-  (lp-print-str (-> system :launchpad) "kotek" :green 500))
---
+++ src/skovajsa/launchpad/lp_print.clj
@@ -0,0 +1,23 @@
+(ns skovajsa.launchpad.lp-print
+  (:require [overtone.midi :as midi]
+            [skovajsa.launchpad.utils :as utils]))
+
+(def char-map
+  {\e [1 2 2 3 4 5 6 17 18 33 34 49 50 50 51 52 53 65 66 81 82 97 98 113 114 114 115 116 117 118]
+   \k [1 2 6 17 18 21 33 34 36 49 50 51 65 66 67 81 82 84 97 98 101 113 114 118]
+   \t [1 2 3 4 5 6 17 18 19 20 21 22 35 36 51 52 67 68 83 84 99 100 115 116]
+   \o [3 4 18 21 33 33 38 38 49 54 65 70 81 86 98 101 115 116]})
+
+(defn lp-print
+  [lp notes vel dur]
+  (doseq [n notes]
+    (midi/midi-note (:rcv lp) n vel dur)))
+
+(defn lp-print-str
+  [lp str color dur]
+  (doseq [c str]
+    (lp-print (:rcv lp) (get char-map c) (utils/color->vel color) dur)
+    (Thread/sleep dur)))
+
+(comment
+  (lp-print-str (-> system :launchpad) "kotek" :green 500))
